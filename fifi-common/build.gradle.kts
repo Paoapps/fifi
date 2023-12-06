7f5b1dc184381959d0833af435b13cf957a5b049
Lammert Westerhoff <westerhoff@gmail.com>
2023-12-06 09:24:31 -0600

    Use blocked cache 0.0.2

--- fifi-common/build.gradle.kts
+++ fifi-common/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.paoapps.fifi"
-version = "0.0.6"
+version = "0.0.7"
 
 kotlin {
     android {
--- fifi-framework/build.gradle.kts
+++ fifi-framework/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.paoapps.fifi"
-version = "0.0.6"
+version = "0.0.7"
 
 val ktorVersion = "2.1.3"
 val logbackVersion = "1.2.3"
