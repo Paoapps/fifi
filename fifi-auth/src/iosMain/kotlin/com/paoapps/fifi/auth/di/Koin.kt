52afc472dbe308a3f7181800a24001c597088a18
Lammert Westerhoff <westerhoff@gmail.com>
2025-01-13 08:24:25 -0600

    Update token store settings to try and prevent crash

--- fifi-auth/src/iosMain/kotlin/com/paoapps/fifi/auth/di/Koin.kt
+++ fifi-auth/src/iosMain/kotlin/com/paoapps/fifi/auth/di/Koin.kt
@@ -9,15 +9,18 @@ import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 import platform.Foundation.CFBridgingRetain
 import platform.Security.kSecAttrAccessible
+import platform.Security.kSecAttrAccessibleAfterFirstUnlock
+import platform.Security.kSecAttrAccessibleWhenUnlocked
 import platform.Security.kSecAttrAccessibleWhenUnlockedThisDeviceOnly
 import platform.Security.kSecAttrService
 
 @OptIn(ExperimentalSettingsImplementation::class, ExperimentalForeignApi::class)
 internal actual fun platformInjections(serviceName: String, module: Module) {
     module.single<Settings>(named(PlatformModuleQualifier.ENCRYPTED_SETTINGS)) {
+        KeychainSettings(Pair(kSecAttrAccessible, kSecAttrAccessibleWhenUnlocked)).clear()
         KeychainSettings(
             kSecAttrService to CFBridgingRetain(serviceName),
-            kSecAttrAccessible to kSecAttrAccessibleWhenUnlockedThisDeviceOnly
+            kSecAttrAccessible to kSecAttrAccessibleAfterFirstUnlock
         )
     }
     module.single<Settings>(named(PlatformModuleQualifier.SETTINGS)) {
