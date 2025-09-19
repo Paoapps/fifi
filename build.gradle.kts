53fd16c764376ad15dda6bdcf15eb107a6947804
Lammert Westerhoff <westerhoff@gmail.com>
2025-09-19 13:53:39 -0600

    Simplify gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,147 @@ plugins {
     kotlin("multiplatform") version libs.versions.kotlin apply false
     id("com.android.library") version libs.versions.plugin.android apply false
     kotlin("plugin.serialization") version libs.versions.kotlin apply false
-    id("com.vanniktech.maven.publish") version "0.34.0" apply false
+    alias(libs.plugins.vanniktech.mavenPublish) apply false
 }
 
 allprojects {
     repositories {
         google()
         mavenCentral()
+    }
+}
+
+// Shared configuration for KMP library modules
+subprojects {
+    if (name.startsWith("fifi-")) {
+        apply(plugin = "kotlin-multiplatform")
+        apply(plugin = "com.android.library")
+        apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
+        apply(plugin = "com.vanniktech.maven.publish")
+
+        group = property("group") as String
+        version = property("version") as String
+
+        extensions.configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
+            androidTarget {
+                compilations.all {
+                    compileTaskProvider.configure {
+                        compilerOptions {
+                            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+                        }
+                    }
+                }
+                publishLibraryVariants("release")
+                publishLibraryVariantsGroupedByFlavor = true
+            }
+
+            iosX64()
+            iosArm64()
+            iosSimulatorArm64()
+            jvm()
+
+            sourceSets.apply {
+                commonMain {
+                    dependencies {
+                        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+                        implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
+                        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+                    }
+                }
+                commonTest {
+                    dependencies {
+                        implementation(kotlin("test"))
+                    }
+                }
+                androidMain {
+                    dependencies {
+                        // Android specific dependencies can be added here
+                    }
+                }
+                androidUnitTest {
+                    dependencies {
+                        implementation(kotlin("test"))
+                    }
+                }
+                iosX64Main.get()
+                iosArm64Main.get()
+                iosSimulatorArm64Main.get()
+                iosMain {
+                    dependsOn(commonMain.get())
+                    iosX64Main.get().dependsOn(this)
+                    iosArm64Main.get().dependsOn(this)
+                    iosSimulatorArm64Main.get().dependsOn(this)
+                }
+                iosX64Test.get()
+                iosArm64Test.get()
+                iosSimulatorArm64Test.get()
+                iosTest {
+                    dependsOn(commonTest.get())
+                    iosX64Test.get().dependsOn(this)
+                    iosArm64Test.get().dependsOn(this)
+                    iosSimulatorArm64Test.get().dependsOn(this)
+                }
+                jvmMain.get()
+                jvmTest.get()
+            }
+        }
+
+        extensions.configure<com.android.build.gradle.LibraryExtension> {
+            compileSdk = 34
+            sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+            defaultConfig {
+                minSdk = 26
+            }
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_11
+                targetCompatibility = JavaVersion.VERSION_11
+            }
+            namespace = "com.paoapps.fifi.${project.name.replace("-", "")}"
+        }
+
+        extensions.configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+            publishToMavenCentral()
+
+            coordinates(
+                group.toString(),
+                project.name,
+                version.toString()
+            )
+
+            pom {
+                name = "FiFi ${project.name.replaceFirstChar { it.uppercase() }}"
+                description = "${project.name.replace("-", " ").replaceFirstChar { it.uppercase() }} module for FiFi Kotlin Multiplatform library."
+                inceptionYear = "2024"
+                url = "https://github.com/Paoapps/fifi"
+                licenses {
+                    license {
+                        name = "MIT"
+                        url = "https://opensource.org/licenses/MIT"
+                        distribution = "repo"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "lammertw"
+                        name = "Lammert Westerhoff"
+                        email = "lammert@paoapps.com"
+                    }
+                }
+                scm {
+                    url = "https://github.com/Paoapps/fifi"
+                    connection = "scm:git:git://github.com/Paoapps/fifi.git"
+                    developerConnection = "scm:git:ssh://git@github.com/Paoapps/fifi.git"
+                }
+            }
+        }
+
+        // Configure signing only when publishing to Maven Central
+        if (project.hasProperty("signingInMemoryKeyId") ||
+            project.hasProperty("signing.keyId") ||
+            System.getenv("GPG_KEY_ID") != null) {
+            extensions.configure<com.vanniktech.maven.publish.MavenPublishBaseExtension> {
+                signAllPublications()
+            }
+        }
     }
 }
--- fifi-framework/build.gradle.kts
+++ fifi-framework/build.gradle.kts
@@ -1,63 +1,22 @@
-plugins {
-    kotlin("multiplatform") version libs.versions.kotlin
-    id("com.android.library") version libs.versions.plugin.android
-    kotlin("plugin.serialization") version libs.versions.kotlin
-    id("com.vanniktech.maven.publish") version "0.34.0"
-}
-
-group = "com.paoapps.fifi"
-version = "0.0.36-SNAPSHOT-41"
-
 kotlin {
-    androidTarget {
-        compilations.all {
-            compileTaskProvider.configure {
-                compilerOptions {
-                    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-                }
-            }
-        }
-        publishLibraryVariants("release")
-        publishLibraryVariantsGroupedByFlavor = true
-    }
-
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-    jvm()
-
     sourceSets {
-        val commonMain by getting {
+        commonMain {
             dependencies {
                 implementation(project(":fifi-common"))
 
                 //Network
                 implementation(libs.bundles.ktor.common)
 
-                //JSON
-                implementation(libs.kotlinx.serialization.json)
-
                 // DI
                 api(libs.koin.core)
-
-                // Date Time
-                implementation(libs.kotlinx.datetime)
-
-                //Coroutines
-                implementation(libs.kotlinx.coroutines.core)
 
                 implementation(libs.multiplatform.settings)
 
                 implementation(libs.blockedcache)
             }
         }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
 
-        val androidMain by getting {
+        androidMain {
             dependencies {
                 implementation(libs.ktor.client.okHttp)
                 implementation(libs.kotlinx.coroutines.core)
@@ -68,92 +27,16 @@ kotlin {
             }
         }
 
-        val androidUnitTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
-
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
+        iosMain {
             dependencies {
                 implementation(libs.ktor.client.ios)
             }
         }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
-        }
-        val jvmMain by getting {
+
+        jvmMain {
             dependencies {
                 implementation(libs.ktor.client.okHttp)
             }
         }
-        val jvmTest by getting
-    }
-}
-
-android {
-    compileSdk = 34
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    defaultConfig {
-        minSdk = 26
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    namespace = "com.paoapps.fifi.framework"
-}
-
-mavenPublishing {
-    publishToMavenCentral()
-
-    coordinates(group.toString(), "fifi-framework", version.toString())
-
-    pom {
-        name = "FiFi Framework"
-        description = "Framework utilities for FiFi Kotlin Multiplatform library."
-        inceptionYear = "2024"
-        url = "https://github.com/Paoapps/fifi"
-        licenses {
-            license {
-                name = "MIT"
-                url = "https://opensource.org/licenses/MIT"
-                distribution = "repo"
-            }
-        }
-        developers {
-            developer {
-                id = "lammertw"
-                name = "Lammert Westerhoff"
-                email = "lammert@paoapps.com"
-            }
-        }
-        scm {
-            url = "https://github.com/Paoapps/fifi"
-            connection = "scm:git:git://github.com/Paoapps/fifi.git"
-            developerConnection = "scm:git:ssh://git@github.com:Paoapps/fifi.git"
-        }
-    }
-}
-
-// Configure signing only when publishing to Maven Central
-if (project.hasProperty("signingInMemoryKeyId") ||
-    project.hasProperty("signing.keyId") ||
-    System.getenv("GPG_KEY_ID") != null) {
-    mavenPublishing {
-        signAllPublications()
     }
 }
